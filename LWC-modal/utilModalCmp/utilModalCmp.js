df187e130e175c51f1e1f6098f793ebed13f43ff
mostafaghonem <mostafaghonem90@yahoo.com>
2023-10-07 13:50:18 +0300

    update LWC-modal

--- LWC-modal/utilModalCmp/utilModalCmp.js
+++ LWC-modal/utilModalCmp/utilModalCmp.js
@@ -1,28 +1,21 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api } from "lwc";
 
 export default class UtilModalCmp extends LightningElement {
-    @api showPositive;
-    @api showNegative;
-    @api positiveButtonLabel = 'Save';
-    @api negativeButtonLable = 'Cancel';
-    @api showModal;
+	@api showPositive;
+	@api showNegative;
+	@api positiveButtonLabel = "Save";
+	@api negativeButtonLable = "Cancel";
+	@api showModal;
 
-    constructor(){
-        super();
-        this.showNegative = true;
-        this.showPositive = true;
-        this.showModal = false;
-    }
+	handlePositive() {
+		this.dispatchEvent(new CustomEvent("positive"));
+	}
 
-    handlePositive(){
-        this.dispatchEvent(new CustomEvent('positive'));
-    }
+	handleNegative() {
+		this.dispatchEvent(new CustomEvent("negative"));
+	}
 
-    handleNegative(){
-        this.dispatchEvent(new CustomEvent('negative'));
-    }
-
-    handleClose(){
-        this.dispatchEvent(new CustomEvent('close'));
-    }
+	handleClose() {
+		this.dispatchEvent(new CustomEvent("close"));
+	}
 }
