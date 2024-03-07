2db0a4f041b151a739ea9826bff2c7a51c23b554
mostafaghonem <mostafaghonem90@yahoo.com>
2024-03-07 20:44:55 +0200

    update the whoOpenRecord component

--- whoOpenRecord/whoOpenRecord.js
+++ whoOpenRecord/whoOpenRecord.js
@@ -1,27 +1,38 @@
-import { LightningElement ,api} from 'lwc';
+import { LightningElement ,api,wire} from 'lwc';
 import Id from "@salesforce/user/Id";
 import { updateRecord } from 'lightning/uiRecordApi';
+import getCaseById from '@salesforce/apex/WhoOpenRecordHelper.getCaseById';
+
 
 export default class WhoOpenRecord extends LightningElement {
-    @api field_Api_Name;
+    @api field_Api_Name="";
     @api recordId
     userId = String(Id);
 
-
     connectedCallback(){
-        const fields ={};
-        fields['Id'] = this.recordId;
-        fields[this.field_Api_Name] = this.userId;
-        const recordInput = { 
-            fields 
-        };
+        getCaseById({recordId:this.recordId , fieldApiName:this.field_Api_Name.toString()})
+        .then((data)=>{
+            console.log(data[this.field_Api_Name]);
+            if(data[this.field_Api_Name] == undefined){
+                const fields ={};
+                fields['Id'] = this.recordId;
+                fields[this.field_Api_Name] = this.userId;
+                const recordInput = { 
+                    fields 
+                };
+        
+                updateRecord(recordInput)
+                .then(()=>{
+                    console.log('Record Updated Successfully')
+                })
+                .catch((err)=>{
+                    console.log(`Error: ${JSON.stringify(err.body.message)}`)
+                })
+            }
 
-        updateRecord(recordInput)
-        .then(()=>{
-            console.log('Record Updated Successfully')
         })
-        .catch((err)=>{
-            console.log(`Error: ${JSON.stringify(err.body.message)}`)
+        .catch((error)=>{
+            console.log (error)
         })
     }
 }
